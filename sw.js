d938dc2d25e064cce9e971b50078b0cf5829655b
khamalputra <khamalputra@users.noreply.github.com>
2026-07-11 21:16:31 +0700

    Use CSS mix-blend-multiply to transparently blend landscape logo white background and bump PWA cache to v5

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'indomarco-sales-v4'; // Naikkan versi cache
+const CACHE_NAME = 'indomarco-sales-v5'; // Naikkan versi cache
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
